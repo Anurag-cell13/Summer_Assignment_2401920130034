d49903299bf1f739dcbcbd93189532a0b21fa0a2
Anurag Sharma <sharmaanurag8273@gmail.com>
2026-06-07 13:49:37 +0530

    update spiral matrix

---
+++ Week-1/Day_4/0054_spiral_matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> brr;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int totalElements = m * n;
+
+        int startingRow = 0;
+        int endingCol = n-1;
+        int endingRow = m-1;
+        int startingCol = 0;
+
+        int count = 0;
+        
+        while(count < totalElements ){
+            for(int i = startingCol; i<= endingCol && count < totalElements; i++){
+                brr.push_back(matrix[startingRow][i]);
+                count++;
+            }
+            startingRow++;
+
+            for(int i=startingRow; i<=endingRow && count < totalElements; i++){
+                brr.push_back(matrix[i][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            for(int i=endingCol; i>=startingCol && count < totalElements; i--){
+                brr.push_back(matrix[endingRow][i]);
+                count++;
+            }
+            endingRow--;
+
+            for(int i=endingRow; i>=startingRow && count < totalElements; i--){
+                brr.push_back(matrix[i][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+
+        return brr;
+        
+    }
+};
